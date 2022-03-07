6cadee979d4c9d2fbfb5e35dd6700e72b1f29c9d
Spencer <sp.gib3@gmail.com>
2022-03-07 17:44:08 +0100

    Created initial file structure

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const { engine } = require('express-handlebars');
+
+const authRoutes = require('./routes/auth');
+const dashboardRoutes = require('./routes/dashboard');
+const errorController = require('./controllers/error');
 
 const app = express();
 
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
 app.use(express.urlencoded({extended: false}));
 
-app.get('/', (req, res) => {
-  res.send("<h1>Hanzi Helper Lives.</h1>")
-});
+app.use(authRoutes);
+app.use(dashboardRoutes);
+
+app.use(errorController.get404);
 
 app.listen(3000, () => {
   console.log('listening');

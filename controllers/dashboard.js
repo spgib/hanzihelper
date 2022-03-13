27c0146ad71207840e2a0e75e683f014b00d7bee
Spencer <sp.gib3@gmail.com>
2022-03-13 15:29:32 +0100

    Added dashboard route/controller/view

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,3 +1,7 @@
 exports.getIndex = (req, res, next) => {
-  res.render('index', { title: '你好'});
+  res.render('index', { title: '你好' });
+};
+
+exports.getDashboard = (req, res, next) => {
+  res.render('./dashboard/dash', { title: 'DASH' });
 };
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -6,4 +6,6 @@ const router = express.Router();
 
 router.get('/', dashboardController.getIndex);
 
+router.get('/dash', dashboardController.getDashboard);
+
 module.exports = router;

b1b18e167ca582834403ae1b3fa08db910156652
Spencer <sp.gib3@gmail.com>
2022-04-02 20:09:22 +0200

    Added timeout/probation logic to learnspace fail button

--- public/js/learn.js
+++ public/js/learn.js
@@ -2,12 +2,14 @@ const hanziLabel = document.querySelector('.learn__front-side h4');
 const meaningLabel = document.querySelector('.meaning h4');
 const pinyinLabel = document.querySelector('.pinyin h4');
 const showAnswerBtn = document.querySelector('.learn__card-controls--front button');
-const nopeBtn = document.querySelectorAll('.learn__card-controls--back button')[0];
+const failBtn = document.querySelectorAll('.learn__card-controls--back button')[0];
 const okBtn = document.querySelectorAll('.learn__card-controls--back button')[1];
 const cardBackside = document.querySelector('.learn__back-side');
 
 const queue = [];
 const pastCards = [];
+let probation = [];
+const probationTimers = [];
 let currentCard;
 
 let initCards, deckId;
@@ -18,13 +20,20 @@ const init = (cardsObject, dId) => {
   initCards.probation.forEach(card => queue.push(card));
   initCards.rev.forEach(card => queue.push(card));
   initCards.remaining.forEach(card => queue.push(card));
-
   nextCard();
 }
 
 const nextCard = () => {
   if (currentCard) {
     pastCards.push(currentCard);
+  }
+
+  if (queue.length === 0) {
+    if (probation.length > 0) {
+      probation.forEach(card => queue.push(card));
+      probation = [];
+      probationTimers.forEach(timer => clearTimeout(timer));
+    }
   }
 
   currentCard = queue.shift();
@@ -53,10 +62,31 @@ const showAnswerHandler = (e) => {
 
 const okButtonHandler = e => {
   flipCard(e.target.closest('div'));
+
+
+
+  nextCard();
+}
+
+const failButtonHandler = e => {
+  flipCard(e.target.closest('div'));
+
+  // report probation to server; if no userCard, create one
+  // push to probation list
+  probation.push(currentCard);
+  // create probation timer and push to timer list
+  const clone = JSON.parse(JSON.stringify(currentCard));
+  clone.probationTimer = setTimeout(() => {
+    queue.unshift(clone);
+    probation = probation.filter(card => card.id !== clone.id);
+  }, 1000 * 15);
+  probationTimers.push(clone.probationTimer);
+
   nextCard();
 }
 
 showAnswerBtn.addEventListener('click', showAnswerHandler);
 okBtn.addEventListener('click', okButtonHandler);
+failBtn.addEventListener('click', failButtonHandler);
 
 export default init;

e5517ee281b20f41d8238039fadd74d68ca41612
Spencer <sp.gib3@gmail.com>
2022-04-03 13:28:48 +0200

    Added fetchHttp function to learn.js

--- public/js/learn.js
+++ public/js/learn.js
@@ -1,9 +1,15 @@
 const hanziLabel = document.querySelector('.learn__front-side h4');
 const meaningLabel = document.querySelector('.meaning h4');
 const pinyinLabel = document.querySelector('.pinyin h4');
-const showAnswerBtn = document.querySelector('.learn__card-controls--front button');
-const failBtn = document.querySelectorAll('.learn__card-controls--back button')[0];
-const okBtn = document.querySelectorAll('.learn__card-controls--back button')[1];
+const showAnswerBtn = document.querySelector(
+  '.learn__card-controls--front button'
+);
+const failBtn = document.querySelectorAll(
+  '.learn__card-controls--back button'
+)[0];
+const okBtn = document.querySelectorAll(
+  '.learn__card-controls--back button'
+)[1];
 const cardBackside = document.querySelector('.learn__back-side');
 
 const queue = [];
@@ -12,16 +18,43 @@ let probation = [];
 const probationTimers = [];
 let currentCard;
 
+const fetchHttp = async (url, method, body) => {
+  try {
+    const token = document
+      .querySelector('meta[name="csrf-token"]')
+      .getAttribute('content');
+
+    const response = await fetch(url, {
+      method,
+      headers: {
+        'Content-Type': 'application/json',
+        'CSRF-Token': token,
+      },
+      body: JSON.stringify(body),
+    });
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      throw new Error(responseData.message);
+    }
+
+    return responseData.message;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 let initCards, deckId;
 const init = (cardsObject, dId) => {
   initCards = cardsObject;
   deckId = dId;
 
-  initCards.probation.forEach(card => queue.push(card));
-  initCards.rev.forEach(card => queue.push(card));
-  initCards.remaining.forEach(card => queue.push(card));
+  initCards.probation.forEach((card) => queue.push(card));
+  initCards.rev.forEach((card) => queue.push(card));
+  initCards.remaining.forEach((card) => queue.push(card));
   nextCard();
-}
+};
 
 const nextCard = () => {
   if (currentCard) {
@@ -30,23 +63,23 @@ const nextCard = () => {
 
   if (queue.length === 0) {
     if (probation.length > 0) {
-      probation.forEach(card => queue.push(card));
+      probation.forEach((card) => queue.push(card));
       probation = [];
-      probationTimers.forEach(timer => clearTimeout(timer));
+      probationTimers.forEach((timer) => clearTimeout(timer));
     }
   }
 
   currentCard = queue.shift();
   updateCardDom(currentCard);
-}
+};
 
-const updateCardDom = cardObj => {
+const updateCardDom = (cardObj) => {
   hanziLabel.innerHTML = cardObj.hanzi;
   meaningLabel.innerHTML = cardObj.meaning;
   pinyinLabel.innerHTML = cardObj.pinyin;
-}
+};
 
-const flipCard = div => {
+const flipCard = (div) => {
   cardBackside.classList.toggle('covered');
   div.classList.add('hidden');
   if (div.nextElementSibling) {
@@ -54,36 +87,35 @@ const flipCard = div => {
   } else {
     div.previousElementSibling.classList.remove('hidden');
   }
-}
+};
 
 const showAnswerHandler = (e) => {
   flipCard(e.target.closest('div'));
-}
+};
 
-const okButtonHandler = e => {
+const okButtonHandler = (e) => {
   flipCard(e.target.closest('div'));
 
-
-
   nextCard();
-}
+};
 
-const failButtonHandler = e => {
+const failButtonHandler = (e) => {
   flipCard(e.target.closest('div'));
 
   // report probation to server; if no userCard, create one
+
   // push to probation list
   probation.push(currentCard);
   // create probation timer and push to timer list
   const clone = JSON.parse(JSON.stringify(currentCard));
   clone.probationTimer = setTimeout(() => {
     queue.unshift(clone);
-    probation = probation.filter(card => card.id !== clone.id);
+    probation = probation.filter((card) => card.id !== clone.id);
   }, 1000 * 15);
   probationTimers.push(clone.probationTimer);
 
   nextCard();
-}
+};
 
 showAnswerBtn.addEventListener('click', showAnswerHandler);
 okBtn.addEventListener('click', okButtonHandler);
